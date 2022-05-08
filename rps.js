74af66179d0cd56aa9bddfdc159711e755218b70
nebil <nebil_pun@hotmail.com>
2022-05-08 01:39:12 -0400

    Solved assigning values to HTML for single match

--- rps.js
+++ rps.js
@@ -5,54 +5,68 @@ let choose = [rock , paper , scissor];
 let compOptions = ['rock' , 'paper' , 'scissor'];
 let player;
 let computer;
-rock.onclick = function(){
-    console.log(rock.name);
-    rand();
+let finalResult;
+let pl = 0;
+let com = 0;
+let ans;
+let result;
+function rand(){
+    let num = Math.floor(Math.random()*3);
+    let compChoose = compOptions[num];
+    return(compChoose);
 }
-paper.onclick = function(){
-    console.log(paper.name);
-    rand();
+rock.onclick = function rock(){
+    player=rock.name;
+    computer=rand();
+    rps(player,computer);
+    console.log(result);
 }
-scissor.onclick = function(){
-    console.log(scissor.name);
-    rand();
+paper.onclick = function paper(){
+    player=paper.name;
+    computer=rand();
+    rps(player,computer);
+    console.log(finalResult);
 }
-function rps(){
-    let a;
-    for(a=1; a <= 5; a++){
+scissor.onclick = function scissor(){
+    player=scissor.name;
+    computer=rand();
+    rps(player,computer);
+    console.log(finalResult);
+}
+function rps(play,comp){
+    cpu.innerHTML=computer;
         if (player===computer){
             result='draw';
         }
         else if (player=='rock'){
             if(computer=='paper'){
             result='Computer wins';
+            com++;
             }
             else{    
             result='Player wins';
+            pl++;
             }
         }
         else if(player=='paper'){
             if(computer=='scissor'){
                 result='Computer wins';
+                com++;
             }
             else{
                 result='Player wins';
+                pl++;
             }
         }
         else if (player='scissor'){
             if(computer=='rock'){
                 result='Computer wins';
+                com++;
             }
             else{
                 result='Player wins';
+                pl++;
             }
-        }   
-        console.log(a);
-    }
-    location.reload();
-}
-function rand(){
-    let num = Math.floor(Math.random()*3);
-    let compChoose = compOptions[num];
-    console.log(compChoose);
+        }
+    final.innerHTML=result;
 }
